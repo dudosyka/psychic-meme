760d3c31d691a2f519d7cb0164781463f5bd1db1
dudosyka <45633581+dudosyka@users.noreply.github.com>
2022-01-18 21:42:44 +0300

    Backend

---
+++ back/main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/time', (req, res, next) => {
+   res.end(String(Date.now()));
+});
+
+app.get('/canClaim', (req, res, next) => {
+    const time = 1642582800000;
+    const now = Date.now();
+    if (time > now) {
+        res.end("false");
+    }
+    res.end("true");
+});
+
+app.listen(3000, () => {
+    console.log('Server start');
+});
